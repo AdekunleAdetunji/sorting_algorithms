3b41cd5ff1010f52978994e4b68aec3a46615870
Olawole Olawale Martins <olawalemart@gmail.com>
2023-07-22 17:57:19 +0100

    task 101 - 102

---
+++ 103-merge_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+void split_n_merge(int *a, int *b, size_t size)
+    /**
+     * merge_sort - ssorts an array of integers in ascending order
+     * using the topdown Merge sort algorithm
+     */
+    void merge_sort(int *array, size_t size)
+{
+    int *dup;
+    unsigned int start, end, middle, i, j;
+
+    dup = malloc(sizeof(int) * size);
+    for (i = 0; i < size; i++)
+        dup[i] = array[i];
+}
--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORT_H
 
 #include <stddef.h>
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -23,5 +24,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 #endif
